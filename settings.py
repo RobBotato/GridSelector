01644c67786fcb7aca4b3344534c071598216131
Robert Li <129627075+RobBotato@users.noreply.github.com>
2023-05-08 21:01:57 -0700

    Settings and Values
    Setting up values of certain variables and importing into main.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+WIDTH, HEIGHT = 1600, 900
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_GRAY = (220, 220, 220)
+
+size = 25
